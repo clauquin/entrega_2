abe6d8839ec3745be2948f26d0c0f617bed1a214
clauquin <clau_quink@yahoo.com.ar>
2013-10-01 16:37:40 -0300

    Update ejercicio1.rb

--- ejercicio1.rb
+++ ejercicio1.rb
@@ -2,74 +2,121 @@ module Animal
       def initialize
       end
       def caminar(n)
-         puts "algo"
+         puts "se mueve"
       end
       def nada
-         puts "algo"
+         puts " esta nadando"
       end
       def desplazarse
         puts 'se desplaza'
       end   
 end
 
-class mamifero
+class Mamifero
+    
+   def algo
+    puts "algo"
+   end
 end
 
-class hombre < mamifero 
+class Hombre < Mamifero 
    include Animal  
    def habla
      puts "hola"
    end  
 end
-class perro < mamifero 
+class Perro < Mamifero 
    include Animal  
    def ladra
-     puts "hola"
+     puts "guau"
    end  
 end
 
-class ave
+class Ave
+    def otro_algo
+    puts "algo"
+   end
 end
-class aguila < ave
+class Aguila < Ave
    include Animal
    def volar
+     puts "vuela"
    end
 end
 
-class reptiles
+class Reptiles
+    def algo_algo
+    puts "algo"
+   end
 end
-class boa < reptiles
+class Boa < Reptiles
    include Animal
    def presiona
      puts "aprieta"
    end  
 end
-class cobra < reptiles
+class Cobra < Reptiles
    include Animal
    def morder
      puts "muerde"
    end  
 end
-class cococdrilo  < reptiles
+class Cococdrilo  < Reptiles
    include Animal
 end
 
-class anfibios
+class Anfibios
+  def algo_frio
+    puts "algo"
+   end
 end
-class sapo < anfibios
+class Sapo < Anfibios
    include Animal
    def saltar
      puts "salta"
    end  
 end
-class peces
+class Peces
+  def algo_mojado
+    puts "algo"
+   end
 end
-class pinguinos < peces
+class Pinguinos < Peces
    include Animal
 end
-class ballena < peces
+class Ballena < Peces
    include Animal  
 end
-class tiburon < peces
+class Tiburon < Peces
    include Animal
 end
+#probando las clases 
+p = Perro.new
+p.ladra
+p.camina
+h = Hombre.new
+h.habla
+h.camina
+h.nada
+ballena = Ballena.new
+ballena.nada
+ping = Pinguino.new
+ping.camina
+ping.nada
+a = Aguila.new
+a.volar
+a.camina
+coco = Cocodrilo.new
+coco.camina
+coco.nada
+b = Boa.new
+b.presiona
+b.desplazarse
+c = Cobra.new
+c.muerde
+c.desplazarse
+s = Sapo.new
+s.saltar
+s.nada
+tib = Tiburon.new
+tiburon.nada
